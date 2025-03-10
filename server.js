8626fdbb6c70c7efa6e795fff648d6f7534854b0
Nawaf <smn11789@gmail.com>
2025-03-11 01:37:06 +0300

    edit 2

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(express.static('public'));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('✅ Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB Connection Error:', err));
 
 // Function to generate a short game ID
