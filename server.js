451653bea08226f352162fc0083199568ad6f197
5inee <99950223+5inee@users.noreply.github.com>
2025-03-10 21:14:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 try {
-    mongoose.connect('mongodb://localhost:27017/prediction-game', {
+    mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => console.log('✅ Connected to MongoDB'))
