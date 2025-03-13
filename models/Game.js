1bf76d591fbb6a3aae82e9ef064343727c8dd88a
Nawaf <smn11789@gmail.com>
2025-03-13 17:33:01 +0300

    hello

--- models/Game.js
+++ models/Game.js
@@ -3,22 +3,8 @@ const mongoose = require('mongoose');
 const gameSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
     question: { type: String, required: true },
-    predictions: {
-        type: Map,
-        of: new mongoose.Schema({
-            content: { type: String, required: true },
-            submittedAt: { type: Date, default: Date.now }
-        })
-    },
-    predictors: {
-        type: Map,
-        of: new mongoose.Schema({
-            id: { type: String, required: true },
-            username: { type: String, required: true },
-            avatarColor: { type: String },
-            joinedAt: { type: Date, default: Date.now }
-        })
-    },
+    predictions: { type: Map, of: Object, default: {} },
+    predictors: { type: Map, of: Object, default: {} },
     maxPredictors: { type: Number, default: 5 },
     revealedToAll: { type: Boolean, default: false }
 });
